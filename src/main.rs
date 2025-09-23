de9d5a2165cd864fabd1f6b1516e6a2551e6a899
Jona <jona.wessendorf1@uni-bielefeld.de>
2025-09-23 13:09:19 +0200

    introduces shell struct for shell state and moves to level function definitions to struct implementations

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,75 @@
-use nix::{sys::{signal::{signal, kill, SigHandler, Signal::{self, SIGTSTP, SIGTTIN, SIGTTOU}}, wait::waitpid}, unistd::{chdir, execvp, fork, getpgrp, getpid, setpgid, tcgetpgrp, tcsetpgrp, write, ForkResult, Pid}};
-use std::io::{self, BufRead, Write};
+use nix::{sys::{signal::{signal, SigHandler, Signal}, wait::waitpid}, unistd::{chdir, execvp, fork, getpgrp, getpid, setpgid, tcsetpgrp, write, ForkResult, Pid}};
+use std::io::{BufRead, Write};
 use std::ffi::CString;
 use std::process::exit;
 use std::env;
 use std::path::PathBuf;
 use regex::Regex;
+
+struct Shell {
+    shell_pid: Pid,
+    last_status: i32,
+    stdin_handle: std::io::Stdin,
+    stdout_handle: std::io::Stdout,
+    // TODO: jobs table
+}
+
+impl Shell {
+    fn new() -> nix::Result<Self> {
+        // ignore signals
+        unsafe {
+            // required when shell process is not foreground and uses tcsetpgrp
+            signal(Signal::SIGTTOU, SigHandler::SigIgn)?;
+            // required for ignoring ctrl-z
+            signal(Signal::SIGTSTP, SigHandler::SigIgn)?;
+        }
+        let shell_pid = getpid();
+        let stdin = std::io::stdin();
+        let stdout = std::io::stdout();
+        setpgid(shell_pid, shell_pid)?;
+        tcsetpgrp(&stdin, shell_pid)?;
+        Ok(Self {
+            last_status: 0,
+            shell_pid: shell_pid,
+            stdin_handle: stdin,
+            stdout_handle: stdout,
+        })
+    }
+
+    fn run(&mut self) -> nix::Result<()> {
+        loop {
+            print!("\n$ ");
+            self.stdout_handle.flush().unwrap();
+
+            let mut input = String::new();
+            let bytes_read = self.stdin_handle.lock().read_line(&mut input).unwrap();
+
+            if bytes_read == 0 {
+                println!("\nexit");
+                exit(0);
+            }
+
+            self.handle_line(input)?;
+
+        }
+    }
+
+    fn handle_line(&mut self, line: String) -> nix::Result<()> {
+        let re = Regex::new(r"\$([a-zA-Z0-9_]+|\$|!)").unwrap();
+
+        let result = re.replace_all(line.as_str(), |captures: &regex::Captures| {
+            let var_name = &captures[1];
+            env::var(var_name).unwrap_or_else(|_| "".to_string())
+        });
+        let line = result.into_owned();
+
+        if let Some(command) = Command::from_input(line) {
+            command.execute();
+        }
+        Ok(())
+    }
+}
+
 
 struct Command {
     // TODO: look into OsString for POSIX compatibility
@@ -13,17 +78,35 @@ struct Command {
 }
 
 impl Command {
+    fn from_input(input: String) -> Option<Self> {
+        // for now we just split at whitespace
+        // TODO: split correctly for quotes etc.
+        let splitted: Vec<&str> = input.as_str().trim().split_whitespace().collect();
+
+        if splitted.is_empty() {
+            return None
+        }
+
+        let cmd = splitted[0].to_string();
+        let args = splitted.iter()
+            .map(|arg| arg.to_string())
+            .collect();
+
+        Some(Self {
+            cmd,
+            args,
+        })
+
+    }
+
     fn cmd_as_cstring(&self) -> CString {
         CString::new(self.cmd.as_str()).unwrap()
     }
+
     fn args_as_cstring(&self) -> Vec<CString> {
         self.args.iter()
             .map(|arg| CString::new(arg.as_str()).unwrap())
             .collect()
-    }
-
-    fn new(cmd: String, args: Vec<String>) -> Self {
-        Self { cmd, args }
     }
 
     fn execute(&self) {
@@ -49,6 +132,8 @@ impl Command {
                     return
                 };
 
+                // TODO: handle cd - with OLDPWD env variable
+                // TODO: set new PWD variable
                 if let Err(e) = chdir(&target) {
                     eprintln!("cd: {}", e);
                     return;
@@ -80,69 +165,7 @@ impl Command {
     }
 }
 
-fn replace_variables(input: String) -> String {
-    let re = Regex::new(r"\$([a-zA-Z0-9_]+|\$|!)").unwrap();
-
-    let result = re.replace_all(input.as_str(), |captures: &regex::Captures| {
-        let var_name = &captures[1];
-        env::var(var_name).unwrap_or_else(|_| "".to_string())
-    });
-    result.into_owned()
-}
-
-
-fn parse_input(input: &str) -> Option<Command> {
-    // for now we just split at whitespace
-    // TODO: split correctly for quotes etc.
-    let splitted: Vec<&str> = input.trim().split_whitespace().collect();
-
-    if splitted.is_empty() {
-        return None
-    }
-
-    let cmd = splitted[0].to_string();
-    let args = splitted.iter()
-        .map(|arg| arg.to_string())
-        .collect();
-
-    Some(Command::new(cmd, args))
-}
-
-
-fn claim_terminal() -> nix::Result<()> {
-    // ignore signals
-    unsafe {
-        // required when shell process is not foreground and uses tcsetpgrp
-        signal(SIGTTOU, SigHandler::SigIgn)?;
-        // required for ignoring ctrl-z
-        signal(SIGTSTP, SigHandler::SigIgn)?;
-    }
-    let shell_pid = getpid();
-    setpgid(shell_pid, shell_pid)?;
-    tcsetpgrp(&std::io::stdin(), shell_pid)?;
-    Ok(())
-}
-
 fn main() {
-    claim_terminal().expect("Failed to get foreground for terminal");
-    loop {
-        print!("\n$ ");
-        io::stdout().flush().unwrap();
-        let stdin = io::stdin();
-        let mut input = String::new();
-        let bytes_read = stdin.lock().read_line(&mut input).unwrap();
-
-        if bytes_read == 0 {
-            println!("\nexit");
-            exit(0);
-        }
-
-        input = replace_variables(input);
-
-        if let Some(command) = parse_input(input.as_str()) {
-            command.execute();
-        } else {
-            continue;
-        }
-    }
+    let mut shell = Shell::new().expect("Failed to spawn shell");
+    shell.run().expect("Failed to run shell");
 }
